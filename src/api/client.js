7524ac3015847aaf17404fb8be93d2aaec5f9535
absalom <absalomsilwal93@gmail.com>
2026-04-12 17:34:40 +1000

    chore: remove node_modules and add it to .gitignore

--- src/api/client.js
+++ src/api/client.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const client = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000',
-  //baseURL: 'https://app-searchly-latest.onrender.com' || 'http://localhost:8000'
+  //baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000',
+  baseURL: 'https://app-searchly-latest.onrender.com' || 'http://localhost:8000'
 });
 
 export default client;
